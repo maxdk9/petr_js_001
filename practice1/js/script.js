27269411a83fb35baf44516bc376d7e05b9ed533
maxdk9 <maxdk9@gmail.com>
2021-07-04 19:03:42 +0300

    chapter15

--- practice1/js/script.js
+++ practice1/js/script.js
@@ -1,3 +1,4 @@
+"use strict"
 const numberOfFilms=+prompt('Сколько фильмов вы посмотрели?','') ;
 const personalMovieDB={
     count:numberOfFilms,
@@ -7,13 +8,48 @@ const personalMovieDB={
     privat:false
 };
 
-const a=prompt('Один из последних просмотренных фильмов? ',''),
-    b=prompt('На сколько оцените его?',''),
-    c=prompt('Один из последних просмотренных фильмов? ',''),
-    d=prompt('На сколько оцените его? ','');
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+
+// for(let i=0;i<2;i++){
+//     const a=prompt('Один из последних просмотренных фильмов? ',''),
+//     b=prompt('На сколько оцените его?','');
+//     if(a!=null&&b!=null&&a!=''&&b!=''&&a.length<50){
+//         personalMovieDB.movies[a]=b;
+//         console.log('done');
+//     }
+//     else{
+//         console.log('error');
+//         i--;
+//     }    
+// }
+let i=0;
+while(i<2){
+    const a=prompt('Один из последних просмотренных фильмов? ',''),
+         b=prompt('На сколько оцените его?','');
+         if(a!=null&&b!=null&&a!=''&&b!=''&&a.length<50){
+             personalMovieDB.movies[a]=b;
+             console.log('done');
+             i++;
+         }
+         else{
+             console.log('error');
+    
+         }    
+}
+
+
+
+if(personalMovieDB.count<10){
+    console.log('you watch few films');
+}
+else if (personalMovieDB.count>=10){
+    console.log('you are experinced kino-user');
+}
+else if(personalMovieDB.count>30){
+    console.log('You are kinoman');
+
+}else{
+    console.log('error');
+}
+
 console.log(personalMovieDB);
-
-
